53e565e79a5e2383eb3874913a67bf16e698b5d6
Ricardo Fontes <163214512+lricardofontes@users.noreply.github.com>
2024-06-18 21:51:44 -0300

    2

--- Funcoes.py
+++ Funcoes.py
@@ -7,7 +7,7 @@ def ler_banco():
     with open('banco_de_dados.json','r') as pacientes:
         return json.load(pacientes)
 
-def salvar(Banco_pacientes):
+def salvar(Banco_pacientes, operacao='cadastrar'):
     '''FUNÇÃO QUE GRAVA OS PACIENTES CADASTRADOS NO BANCO DE DADOS'''
     try:
         with open('banco_de_dados.json','w') as pacientes:
@@ -15,26 +15,41 @@ def salvar(Banco_pacientes):
     except :
         print('Erro ao salvar dados no banco.')
     else:
-        print('Paciente cadastrado com sucesso! ')
+        if operacao == 'cadastrar':
+            print('Paciente cadastrado com sucesso! ')
+        elif operacao == 'excluir':
+            print('Paciente excluído com sucesso!')
 
+
+def calcular_imc(peso, altura):
+    '''FUNÇÃO QUE CALCULA O IMC DO PACIENTE'''
+    imc = peso / (altura ** 2)
+    return round(imc, 2)  # Arredonda o IMC para duas casas decimais
 
 def adicionar_paciente():
     '''FUNÇÃO QUE ADCIONA PACIENTES AO BANCO DE DADOS'''
     
     nome = str(input('Digite o nome do(a) Paciente: '))
-    idade = int(input('Digite o idade do(a) Paciente: '))
-    peso = float(input('Digite o peso do(a) Paciente: '))
+    idade = int(input('Digite a idade do(a) Paciente: '))
+    peso = float(input('Digite o peso do(a) Paciente (em kg): '))
+    altura = float(input('Digite a altura do(a) Paciente (em metros): '))
     bf = float(input('Digite o bf do(a) Paciente: '))
     objetivo = str(input('Digite o objetivo do(a) Paciente: '))
     
+    imc = calcular_imc(peso, altura)
    
     Banco_pacientes = ler_banco()
     Banco_pacientes['Nome'].append(nome)
     Banco_pacientes['Idade'].append(idade)
     Banco_pacientes['Peso'].append(peso)
+    Banco_pacientes['Altura'].append(altura)
     Banco_pacientes['BF'].append(bf)
     Banco_pacientes['Objetivo'].append(objetivo)
+    Banco_pacientes['IMC'].append(imc)
+    
     salvar(Banco_pacientes)
+
+# Continue com as funções 'listar_pacientes' e 'excluir_paciente' e certifique-se de que elas também lidam com os novos campos 'Altura' e 'IMC'.
 
 
 def listar_pacientes():
@@ -46,13 +61,33 @@ def listar_pacientes():
     for i in tqdm(range(3), colour='MAGENTA'):
         time.sleep(1)
         
-    for linha in range(qtd_linhas):
-        print(f'\n\nPACIENTE: {Banco_pacientes["Nome"][linha]}')
-        print(f'IDADE: {Banco_pacientes["Idade"][linha]}')
-        print(f'PESO: {Banco_pacientes["Peso"][linha]}')
-        print(f'BF: {Banco_pacientes["BF"][linha]}')
-        print(f'OBJETIVO: {Banco_pacientes["Objetivo"][linha]}')
+    for i in range(qtd_linhas):
+        print(f'{i+1}. {Banco_pacientes["Nome"][i]} - {Banco_pacientes["Idade"][i]} anos - {Banco_pacientes["Peso"][i]} kg - {Banco_pacientes["Altura"][i]} m - IMC: {Banco_pacientes["IMC"][i]}% - BF: {Banco_pacientes["BF"][i]}% - Objetivo: {Banco_pacientes["Objetivo"][i]}')
+
         
+def excluir_paciente():
+    '''FUNÇÃO QUE EXCLUI UM PACIENTE DO BANCO DE DADOS'''
+    Banco_pacientes = ler_banco()
+    qtd_linhas = len(Banco_pacientes['Nome'])
+    
+    # Listar os pacientes
+    print('LISTA DE PACIENTES:\n')
+    for i in range(qtd_linhas):
+        print(f'{i+1}. {Banco_pacientes["Nome"][i]} - {Banco_pacientes["Idade"][i]} anos - {Banco_pacientes["Peso"][i]} kg - {Banco_pacientes["Altura"][i]} m - IMC: {Banco_pacientes["IMC"][i]}% - BF: {Banco_pacientes["BF"][i]}% - Objetivo: {Banco_pacientes["Objetivo"][i]}')
+    
+    # Solicitar ao usuário que escolha um paciente para excluir
+    try:
+        escolha = int(input('\nDigite o número do paciente que deseja excluir: ')) - 1
+        if 0 <= escolha < qtd_linhas:
+            # Excluir os dados do paciente escolhido
+            for chave in Banco_pacientes:
+                del Banco_pacientes[chave][escolha]
+            salvar(Banco_pacientes, operacao='excluir')
+            
+        else:
+            print('Número inválido. Por favor, escolha um número da lista.')
+    except ValueError:
+        print('Por favor, digite um número válido.')
 
 
 
--- main.py
+++ main.py
@@ -8,15 +8,17 @@ print('=======BEM VINDO!!=======\n***** NUTRIFELA *****\n')
 escolha = {
    '1': Funcoes.adicionar_paciente,
    '2': Funcoes.listar_pacientes,
+   '3': Funcoes.excluir_paciente
 }
 
 while True:
     print('\n\nDIGITE O NÚMERO CORRESPONDENTE A UMA OPÇÃO ABAIXO: ')
     print('1 - CADASTRAR PACIENTES')
     print('2 - LISTAR PACIENTES')
-    print('3 - SAIR')
+    print('3 - EXCLUIR PACIENTE')
+    print('4 - SAIR')
     opcao = input('-> ')
-    if opcao == '3':
+    if opcao == '4':
         exit()
     elif opcao not in ['1','2','3',]:
         print('\nDigite uma opção válida!')
